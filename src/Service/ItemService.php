9b0fc19e128ef34728e18a8b1404d8ad77bfc7ad
vasyldmutryk <dmutrykv@gmail.com>
2021-06-22 21:16:08 +0300

    add getById and update item repo methods

--- src/Service/ItemService.php
+++ src/Service/ItemService.php
@@ -4,15 +4,19 @@ namespace App\Service;
 
 use App\Entity\Item;
 use App\Entity\User;
+use App\Repository\ItemRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class ItemService
 {
     private $entityManager;
+    /** @var ItemRepository $itemRepository */
+    private $itemRepository;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, ItemRepository $itemRepository)
     {
         $this->entityManager = $entityManager;
+        $this->itemRepository = $itemRepository;
     }
 
     public function create(User $user, string $data): void
@@ -23,5 +27,18 @@ class ItemService
 
         $this->entityManager->persist($item);
         $this->entityManager->flush();
+    }
+
+    public function update(Item $item, string $data): void
+    {
+        $item->setData($data);
+
+        $this->entityManager->persist($item);
+        $this->entityManager->flush();
+    }
+    
+    public function getById(int $id): ?Item
+    {
+        return $this->itemRepository->findOneBy(['id' => $id]);
     }
 } 
