16b7ae3075e4e325f77d8fd986336024df0c21ae
vasyldmutryk <dmutrykv@gmail.com>
2021-06-22 21:12:42 +0300

    add test for update method

--- tests/unit/Service/ItemServiceTest.php
+++ tests/unit/Service/ItemServiceTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Unit;
 
 use App\Entity\Item;
 use App\Entity\User;
+use App\Repository\ItemRepository;
 use App\Service\ItemService;
 use PHPUnit\Framework\TestCase;
 use Doctrine\ORM\EntityManagerInterface;
@@ -25,8 +26,9 @@ class ItemServiceTest extends TestCase
     {
         /** @var EntityManagerInterface */
         $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $itemRepository = $this->createMock(ItemRepository::class);
         
-        $this->itemService = new ItemService($this->entityManager);
+        $this->itemService = new ItemService($this->entityManager, $itemRepository);
     }
 
     public function testCreate(): void
@@ -36,10 +38,27 @@ class ItemServiceTest extends TestCase
         $data = 'secret data';
 
         $expectedObject = new Item();
-        $expectedObject->setUser($user);
+        $expectedObject->setUser($user)->setData($data);
 
         $this->entityManager->expects($this->once())->method('persist')->with($expectedObject);
 
         $this->itemService->create($user, $data);
+    }
+
+    public function testUpdate(): void
+    {
+        /** @var User */
+        $user = $this->createMock(User::class);
+        $data = 'secret data';
+
+        $expectedObject = new Item();
+        $expectedObject->setUser($user)->setData($data);
+        
+        $this->itemService->create($user, $data);
+        
+        $newData = 'updated data';
+        $this->entityManager->expects($this->once())->method('persist')->with($expectedObject);
+
+        $this->itemService->update($expectedObject, $newData);
     }
 }
